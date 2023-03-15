1c9ff039960e304e710f86c35cf2367735ac5f5b
EnioAmarantes <enio.amarantes@gmail.com>
2023-03-15 20:43:10 -0300

    Spaceship player and enemie

---
+++ Assets/scriptNPC.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptNPC : MonoBehaviour
+{
+    private Rigidbody2D rbd;
+    public float vel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vel = 5;
+        rbd = GetComponent<Rigidbody2D>();
+        rbd.velocity = new Vector2(0, -vel);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -Camera.main.orthographicSize)
+            Destroy(this.gameObject);
+        
+    }
+}
---
+++ Assets/scriptPc.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptPc : MonoBehaviour
+{
+    private float altura;
+    private float largura;
+
+    private Rigidbody2D rbd;
+    public float vel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        altura = Camera.main.orthographicSize;
+        largura = altura * Camera.main.aspect;
+        vel = 10;
+        rbd = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        rbd.velocity = new Vector2(x, y) * vel;
+
+        if (transform.position.x > largura)
+            transform.position = 
+                  new Vector2(-largura,transform.position.y);
+        else if(transform.position.x < -largura)
+            transform.position =
+                  new Vector2(largura, transform.position.y);
+
+        if (transform.position.y > 0)
+            transform.position =
+                    new Vector2(transform.position.x, 0);
+        else if(transform.position.y < -altura)
+            transform.position =
+                    new Vector2(transform.position.x, -altura);
+
+    }
+}
